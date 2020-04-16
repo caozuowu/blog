c3910f817fb4a2e840bfcebf697f9d80f4f6df5f
zuowu <zuowu@staff.weibo.com>
2020-04-16 19:32:11 +0800

    no message

--- _posts/leetcode/code.js
+++ _posts/leetcode/code.js
@@ -1,19 +1,43 @@
 
+function find(s) {
+    var mark = []
+    for (var x in s) {
+        mark.push(new Array(s.length))
+        mark[x][x] = 1
+    }
 
-function find(s, i, j) {
-    var n = Math.floor((j+i)/2)
-    console.log(n)
-    var mark = [s[n]]
-    //left
-    for (var i = n; i <= j ; i++) {
-        for (var j = 0; j <= mark.length-1; j++) {
-            if (s[i] == mark[j]) {
-                break;
+   
+    for (var l = 1; l < s.length; l++) {
+        for (var i = 0; i < s.length - l; i++) {
+            var j = i + l
+            if (s[i] == s[j]) {
+                var left = i
+                var right = j
+                if (j == i+1) {
+                    mark[i][j] = 2
+                }else {
+                    mark[i][j] = mark[i+1][j-1] + 2
+                }
+                
+            }else {
+                mark[i][j] = Math.max(mark[i+1][j], mark[i][j-1])
             }
         }
     }
-    //right
+
+
+    for (var x of mark) {
+        var s = ''
+        for (var n of x) {
+            s += n + ' '
+        }
+        console.log(s)
+    }
+
+
 }
 
-var s = '3kdkjgooe'
-var r = find(s)
+// var s = 'javaej'
+// var s = 'aaaaaa'
+var s = 'character'
+find(s)
