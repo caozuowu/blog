5374c6d5ab539c035aaba92eef09e3ee0289196e
zuowu <zuowu@staff.weibo.com>
2020-08-04 15:31:53 +0800

    update

--- _posts/leetcode/borad.js
+++ _posts/leetcode/borad.js
@@ -1,16 +1,31 @@
 
-
-var isPalindrome = function (x) {
-
-    var reverted = 0;
-    while (x > reverted) {
-        reverted = reverted*10 + x%10;
-        x = x/10;
+var mark = [
+    {value: 1000, symbol: "M"},
+    {value: 900,  symbol: "CM"},
+    {value: 500,  symbol: "D"},
+    {value: 400,  symbol: "CD"},
+    {value: 100,  symbol: "C"},
+    {value: 90,   symbol: "XC"},
+    {value: 50,   symbol: "L"},
+    {value: 40,   symbol: "XL"},
+    {value: 10,   symbol: "X"},
+    {value: 9,    symbol: "IX"},
+    {value: 5,    symbol: "V"},
+    {value: 4,    symbol: "IV"},
+    {value: 1,    symbol: "I"},
+]
+    
+var intToRoman = function(num) {
+    var result = "";
+    for (var i = 0; i < mark.length; i++) {
+        var value = mark[i].value
+        while (value <= num) {
+            num -= value;
+            result += mark[i].symbol
+        }
     }
-
-    console.log(reverted, x);
-
+    console.log(result)
+    return result;
 }
 
-isPalindrome(1234554321)
-
+intToRoman(1994)
