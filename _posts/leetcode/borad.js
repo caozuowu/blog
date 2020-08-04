8e8ba298dd09745b196ed6c6a74bb3e329b1ad4c
zuowu <zuowu@staff.weibo.com>
2020-08-04 12:00:16 +0800

    insert

--- _posts/leetcode/borad.js
+++ _posts/leetcode/borad.js
@@ -1,35 +1,16 @@
 
 
-var subString = function (s) {
-    var mark = [];
-    for (var i = 0; i < s.length; i ++) {
-        mark.push(new Array(s.length))
+var isPalindrome = function (x) {
+
+    var reverted = 0;
+    while (x > reverted) {
+        reverted = reverted*10 + x%10;
+        x = x/10;
     }
 
-    var result = "";
+    console.log(reverted, x);
 
-    for (var l = 0; l < s.length; l++) {
-        for (var i = 0; i < s.length - l; i++) {
-            var j = i + l;
-            if (l == 0) {
-                mark[i][j] = true;
-            }else if (l == 1) {
-                mark[i][j] = s[i] == s[j];
-            }else {
-                mark[i][j] = mark[i+1][j-1] && (s[i] == s[j]);
-            }
-
-            if (mark[i][j] && l+1 > result.length) {
-                result = s.substring(i,j+1);
-            }
-        }
-    }
-
-    console.log(mark)
-    console.log(result)
-
-    return result;
 }
 
+isPalindrome(1234554321)
 
-subString("babad");
