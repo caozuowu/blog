f55b6b043d7b7ff12a2cbb305d5d5d1325fa1fec
zuowu <zuowu@staff.weibo.com>
2020-08-09 14:53:33 +0800

    update

--- _posts/leetcode/borad.js
+++ _posts/leetcode/borad.js
@@ -1,29 +1,54 @@
 
 
-var searchRange = function (target, nums) {
+var result = []
 
-    var lowLeft = 0;
-    var lowRight = nums.length-1;
-
-    var low = 0;
-
-    while (lowLeft < lowRight) {
-        var lowMid = Math.floor((lowRight-lowLeft)/2);
-        if (target == nums[lowMid]) {
-            low = lowMid;
-            break;
+var INSERTION_SORT = function(A) {
+    for (var i = 1; i < A.length; i++) {
+        var key = A[i]
+        //把 A[j] 插入到前面数组中合适的位置
+        var j = i - 1
+        while (j >= 0 && A[j] > key) {
+            A[j+1] = A[j]
+            j = j - 1
         }
-        if (target < nums[lowMid]) {
-            lowRight = lowMid-1
-        }else {
-            lowLeft = lowMid+1
-        }
+        A[j+1] = key
     }
-
-    console.log(low)
-
 }
 
-var nums = [5,7,7,8,8,10]
-var target = 8
-searchRange(target, nums)
+var deepSelected = function (array, candidates, target, start) {
+
+    if (target <= 0) {
+        if (target == 0) {
+            console.log(array);
+            result.push([].concat(array))
+        }
+        return;
+    }
+
+    for (var i = start; i < candidates.length; i ++) {
+        if (target < candidates[i]) {
+            continue
+        }
+        if (i > start && candidates[i] == candidates[i-1]) {
+            continue;
+        }
+        array.push(candidates[i])
+        deepSelected(array , candidates, target-candidates[i], i+1)
+        array.pop()
+    }
+}
+
+
+var combinationSum = function(candidates, target) {
+    INSERTION_SORT(candidates)
+    console.log(candidates)
+    deepSelected([], candidates, target, 0);
+    console.log('result = ', result);
+
+};
+
+
+
+
+combinationSum([10,1,2,7,6,1,5], 8)
+
